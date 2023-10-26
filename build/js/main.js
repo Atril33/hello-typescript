703a3abd21958f5f56f6fd48fb25f649f6aba1f6
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-10-26 15:35:08 +0500

    Add First Line Of Typescript Code

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let firstName;
+let lastName;
+let age;
+firstName = 'Syed';
+lastName = 'Atril';
+age = 20;
+console.log(`My Name is ${firstName} ${lastName} & I am ${age} year old`);
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let firstName: string;
+let lastName: string;
+let age: number;
+firstName = 'Syed'
+lastName = 'Atril'
+age = 20
+
+console.log(`My Name is ${firstName} + ${lastName} I am ${age} year old`)
